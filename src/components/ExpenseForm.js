0e78eb7a1c8603dd907efbfa23af0152bbec616e
Pitipon Pluemworasawat <pitipon@gmail.com>
2019-01-13 00:43:01 +0800

    add calendar from airbnb

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 class ExpenseForm extends React.Component {
 
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
     }
 
     onDescriptionChange = (e) => {
@@ -44,6 +52,14 @@ class ExpenseForm extends React.Component {
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt} // momentPropTypes.momentObj or null
+                        onDateChange={ createdAt => this.setState({ createdAt })} // PropTypes.func.isRequired
+                        focused={this.state.calendarFocused} // PropTypes.bool
+                        onFocusChange={({ focused }) => this.setState({ calendarFocused: focused })} // PropTypes.func.isRequired
+                        numberOfMonths={1}
+                        isOutsideRange={ day => false}
                     />
                     <textarea
                         placeholder="Add a note for your expense (optional)"
